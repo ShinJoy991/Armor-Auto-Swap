91b3213d94ad6947b65edef0a01815990567c8b9
ShinJoy991 <shinjoytestmail@gmail.com>
2024-05-23 02:49:22 +0700

    first 1.20.1 push

---
+++ src/main/java/com/github/shinjoy991/armorautoswap/client/ClientKeyMapping.java
@@ -0,0 +1,17 @@
+package com.github.shinjoy991.armorautoswap.client;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import net.minecraftforge.fml.common.Mod;
+import org.lwjgl.glfw.GLFW;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, value = {Dist.CLIENT})
+public class ClientKeyMapping {
+    public static final String KEY_CATEGORY_RELOAD = "key.category.armorautoswap";
+    public static final String KEY_DESCRIPTION = "key.category.armorautoswap.reload";
+    public static final KeyMapping DRINKING_KEY = new KeyMapping(KEY_DESCRIPTION,
+            KeyConflictContext.IN_GAME, InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_0,
+            KEY_CATEGORY_RELOAD);
+}
