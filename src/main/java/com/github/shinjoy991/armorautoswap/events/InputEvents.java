5fb901ddefacc535d61b60b4e83a7379916236da
ShinJoy991 <shinjoytestmail@gmail.com>
2024-05-27 17:30:02 +0700

    update server compat

--- src/main/java/com/github/shinjoy991/armorautoswap/events/InputEvents.java
+++ src/main/java/com/github/shinjoy991/armorautoswap/events/InputEvents.java
@@ -1,24 +1,23 @@
-package com.github.shinjoy991.armorautoswap.client;
+package com.github.shinjoy991.armorautoswap.events;
 
 import com.github.shinjoy991.armorautoswap.ArmorAutoSwap;
 import com.github.shinjoy991.armorautoswap.Config;
+import com.github.shinjoy991.armorautoswap.client.ArmorSwapPacket;
+import com.github.shinjoy991.armorautoswap.client.ClientKeyMapping;
+import com.github.shinjoy991.armorautoswap.client.NetworkHandler;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-import java.util.HashMap;
 import java.util.UUID;
 
 @Mod.EventBusSubscriber(modid = ArmorAutoSwap.MOD_ID, value = Dist.CLIENT)
 public class InputEvents {
-
-    public static final HashMap<UUID, Boolean> armorSwapEnabled = new HashMap<>();
 
     @SubscribeEvent
     public static void onKeyInput(InputEvent.Key event) {
@@ -26,30 +25,22 @@ public class InputEvents {
             Minecraft minecraft = Minecraft.getInstance();
             if (minecraft.player != null) {
                 UUID playerUUID = minecraft.player.getUUID();
-                boolean isEnabled = armorSwapEnabled.getOrDefault(playerUUID, Config.defaultMode);
-                isEnabled = !isEnabled;
-                armorSwapEnabled.put(playerUUID, isEnabled);
+                boolean currentEnabledState =
+                        DamageEvents.armorSwapEnabled.getOrDefault(playerUUID, Config.defaultMode);
+                boolean isEnabled = !currentEnabledState;
 
-                Component message;
+                NetworkHandler.CHANNEL.sendToServer(new ArmorSwapPacket(playerUUID, isEnabled));
+
+                net.minecraft.network.chat.Component message;
                 if (isEnabled) {
-                    message = Component.literal("Armor swap mode ")
-                            .append(Component.literal("enabled").withStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)));
+                    message = net.minecraft.network.chat.Component.literal("Armor swap mode ")
+                            .append(net.minecraft.network.chat.Component.literal("enabled").withStyle(Style.EMPTY.withColor(ChatFormatting.GREEN)));
                 } else {
-                    message = Component.literal("Armor swap mode ")
+                    message = net.minecraft.network.chat.Component.literal("Armor swap mode ")
                             .append(Component.literal("disabled").withStyle(Style.EMPTY.withColor(ChatFormatting.RED)));
                 }
-
                 minecraft.gui.setOverlayMessage(message, false);
             }
-        }
-    }
-
-    @Mod.EventBusSubscriber(modid = ArmorAutoSwap.MOD_ID, value = Dist.CLIENT, bus =
-            Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void clientSetup(RegisterKeyMappingsEvent event) {
-            event.register(ClientKeyMapping.DRINKING_KEY);
         }
     }
 }
